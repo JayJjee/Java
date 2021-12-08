da5952f872315e5d5f494f9eeb48ca31519f9908
Joan <guigomene15@gmail.com>
2021-12-08 17:38:48 -0300

    Name and age

---
+++ ex02.java
@@ -0,0 +1,12 @@
+public class ex02 {
+    public static void main(String[] args){
+
+        int idade = 21;
+        String nome = "Joan";
+
+        System.out.println("ex02");
+        System.out.print("ex02\n");
+        System.out.printf("Letras: %s\nNumero: %d\n", "ex", 02);
+        System.out.printf("Nome: %s Idade: %d\n", nome, idade);
+    }
+}
