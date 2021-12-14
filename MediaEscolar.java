d4abe7024f90c82ae490fbfa77f261e98610c947
Joan <guigomene15@gmail.com>
2021-12-14 18:30:58 -0300

    Media Escolar

---
+++ MediaEscolar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MediaEscolar {
+    public static void main(String[] args) {
+        System.out.println("MEDIA ESCOLAR");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        int nota1 = scan.nextInt();
+
+        System.out.println("Digite a segunda nota: ");
+        int nota2 = scan.nextInt();
+
+        float media = (nota1 + nota2)/2;
+
+        System.out.printf("A media final eh %.1f.\n", media);
+        if(media >= 6){
+            System.out.println("Aprovado");
+        } else if(media < 6 && media >= 3){
+            System.out.println("Recuperacao");
+        } else{
+            System.out.println("Reprovado");
+        }
+    }
+}
---
+++ ex05.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ex05 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int n1 = 0, n2 = 0, resultado = 0;
+        String nome = "";
+        
+        System.out.println("Digite seu nome: ");
+        nome = scan.nextLine();
+
+        System.out.println("Digite um numero: ");
+        n1 = scan.nextInt();
+        
+        System.out.println("Digite outro numero: ");
+        n2 = scan.nextInt();
+
+        resultado = n1 + n2;
+        
+        System.out.printf("%s, a soma de %d com %d eh igual a %d",nome,n1,n2,resultado);
+    }
+}
